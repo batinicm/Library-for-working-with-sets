d3284f972c0219b3b04b68502a7f515f4e82d86d
Milica Batinic <42036476+batinicm@users.noreply.github.com>
2018-08-06 11:22:52 +0200

    Update PerformanceCalculator.h

--- PerformanceCalculator.h
+++ PerformanceCalculator.h
@@ -1,188 +1,178 @@
-#include "Set.h"
-#include "List.h"
-#include "BST.h"
-#include "Errs.h"
-#include "PerformanceCalculator.h"
-#include <iomanip>
-#include <string>
-
-
-double* Formation(List * l, List* lpom, BST* b, BST *bpom, int tr) {						//formation of a list and bst made up of same numbers so that
-	PerformanceCalculator pc;
-	MTRand_int32 irand;																		//the performance measurements are accurate
-	double* avg = new double[2];
-	avg[0] = 0; avg[1] = 0;
-	int* arr = new int[tr];
-	for (int k = 0; k < 3; k++) {
-	for(int i=0; i<tr; i++) arr[i]= irand();
-		pc.start();
-		for (int j = 0; j<tr; j++) {
-			try {
-				if (k == 1) lpom->Insert(arr[j]);
-				else l->Insert(arr[j]);
-			}
-			catch (ErrElExists g) {}
-		}
-		pc.stop();
-		avg[0] += pc.elapsedMillis();
-		if (k == 0) l->DeleteAll();
-		pc.start();
-		for (int j = 0; j<tr; j++) {
-			try {
-				if (k == 1) bpom->Insert(arr[j]);
-				else b->Insert(arr[j]);
-			}
-			catch (ErrElExists g) {}
-		}
-		pc.stop();
-		avg[1] += pc.elapsedMillis();
-		if (k == 0) b->DeleteAll();
+#ifndef PERFORMANCE_CALCULATOR_H__
+#define PERFORMANCE_CALCULATOR_H__
+#include "windows.h"
+class PerformanceCalculator {
+	LARGE_INTEGER startCounter;
+	LARGE_INTEGER stopCounter;
+	LARGE_INTEGER frequency;
+public:
+	PerformanceCalculator() {
+		startCounter.QuadPart = stopCounter.QuadPart = 0;
+		QueryPerformanceFrequency(&frequency);
 	}
-	delete arr;
-	avg[0] /= 3;
-	avg[1] /= 3;
-	return avg;
+	void start() {
+		QueryPerformanceCounter(&startCounter);
+	}
+	void stop() {
+		QueryPerformanceCounter(&stopCounter);
+	}
+	double elapsedMillis() const {
+		return (stopCounter.QuadPart - startCounter.QuadPart)*1000. / frequency.QuadPart;
+	}
+};
+
+class MTRand_int32 { // Mersenne Twister random number generator
+public:
+	// default constructor: uses default seed only if this is the first instance
+	MTRand_int32() { if (!init) seed(5489UL); init = true; }
+	// constructor with 32 bit int as seed
+	MTRand_int32(unsigned long s) { seed(s); init = true; }
+	// constructor with array of size 32 bit ints as seed
+	MTRand_int32(const unsigned long* array, int size) { seed(array, size); init = true; }
+	// the two seed functions
+	void seed(unsigned long); // seed with 32 bit integer
+	void seed(const unsigned long*, int size); // seed with array
+											   // overload operator() to make this a generator (functor)
+	unsigned long operator()() { return rand_int32(); }
+	// 2007-02-11: made the destructor virtual; thanks "double more" for pointing this out
+	virtual ~MTRand_int32() {} // destructor
+protected: // used by derived classes, otherwise not accessible; use the ()-operator
+	unsigned long rand_int32(); // generate 32 bit random integer
+private:
+	static const int n = 624, m = 397; // compile time constants
+									   // the variables below are static (no duplicates can exist)
+	static unsigned long state[n]; // state vector array
+	static int p; // position in state array
+	static bool init; // true if init function is called
+					  // private functions used to generate the pseudo random numbers
+	unsigned long twiddle(unsigned long, unsigned long); // used by gen_state()
+	void gen_state(); // generate new state
+					  // make copy constructor and assignment operator unavailable, they don't make sense
+	MTRand_int32(const MTRand_int32&); // copy constructor not defined
+	void operator=(const MTRand_int32&); // assignment operator not defined
+};
+
+// inline for speed, must therefore reside in header file
+inline unsigned long MTRand_int32::twiddle(unsigned long u, unsigned long v) {
+	return (((u & 0x80000000UL) | (v & 0x7FFFFFFFUL)) >> 1)
+		^ ((v & 1UL) * 0x9908B0DFUL);
+	// 2013-07-22: line above modified for performance according to http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/Ierymenko.html
+	// thanks Vitaliy FEOKTISTOV for pointing this out
 }
 
-double Insertion(Set* s, int a) {
-	PerformanceCalculator pc;
-	double avg1 = 0;
-	for (int k = 0; k < 3; k++) {
-		pc.start();
-		try {
-			s->Insert(a);
-		}
-		catch (ErrElExists g) {}
-		pc.stop();
-		avg1 += pc.elapsedMillis();
-	}
-	return avg1/3;
+inline unsigned long MTRand_int32::rand_int32() { // generate 32 bit random int
+	if (p == n) gen_state(); // new state vector needed
+							 // gen_state() is split off to be non-inline, because it is only called once
+							 // in every 624 calls and otherwise irand() would become too big to get inlined
+	unsigned long x = state[p++];
+	x ^= (x >> 11);
+	x ^= (x << 7) & 0x9D2C5680UL;
+	x ^= (x << 15) & 0xEFC60000UL;
+	return x ^ (x >> 18);
 }
 
-double Removal(Set* s, int a) {
-	PerformanceCalculator pc;
-	double avg1 = 0;
-	for (int k = 0; k < 3; k++) {
-		pc.start();
-		try {
-			s->Remove(a);
-		}
-		catch (ErrNoElement g) {}
-		pc.stop();
-		avg1 += pc.elapsedMillis();
+// generates double floating point numbers in the half-open interval [0, 1)
+class MTRand : public MTRand_int32 {
+public:
+	MTRand() : MTRand_int32() {}
+	MTRand(unsigned long seed) : MTRand_int32(seed) {}
+	MTRand(const unsigned long* seed, int size) : MTRand_int32(seed, size) {}
+	~MTRand() {}
+	double operator()() {
+		return static_cast<double>(rand_int32()) * (1. / 4294967296.);
+	} // divided by 2^32
+private:
+	MTRand(const MTRand&); // copy constructor not defined
+	void operator=(const MTRand&); // assignment operator not defined
+};
+
+// generates double floating point numbers in the closed interval [0, 1]
+class MTRand_closed : public MTRand_int32 {
+public:
+	MTRand_closed() : MTRand_int32() {}
+	MTRand_closed(unsigned long seed) : MTRand_int32(seed) {}
+	MTRand_closed(const unsigned long* seed, int size) : MTRand_int32(seed, size) {}
+	~MTRand_closed() {}
+	double operator()() {
+		return static_cast<double>(rand_int32()) * (1. / 4294967295.);
+	} // divided by 2^32 - 1
+private:
+	MTRand_closed(const MTRand_closed&); // copy constructor not defined
+	void operator=(const MTRand_closed&); // assignment operator not defined
+};
+
+// generates double floating point numbers in the open interval (0, 1)
+class MTRand_open : public MTRand_int32 {
+public:
+	MTRand_open() : MTRand_int32() {}
+	MTRand_open(unsigned long seed) : MTRand_int32(seed) {}
+	MTRand_open(const unsigned long* seed, int size) : MTRand_int32(seed, size) {}
+	~MTRand_open() {}
+	double operator()() {
+		return (static_cast<double>(rand_int32()) + .5) * (1. / 4294967296.);
+	} // divided by 2^32
+private:
+	MTRand_open(const MTRand_open&); // copy constructor not defined
+	void operator=(const MTRand_open&); // assignment operator not defined
+};
+
+// generates 53 bit resolution doubles in the half-open interval [0, 1)
+class MTRand53 : public MTRand_int32 {
+public:
+	MTRand53() : MTRand_int32() {}
+	MTRand53(unsigned long seed) : MTRand_int32(seed) {}
+	MTRand53(const unsigned long* seed, int size) : MTRand_int32(seed, size) {}
+	~MTRand53() {}
+	double operator()() {
+		return (static_cast<double>(rand_int32() >> 5) * 67108864. +
+			static_cast<double>(rand_int32() >> 6)) * (1. / 9007199254740992.);
 	}
-	return avg1 / 3;
+private:
+	MTRand53(const MTRand53&); // copy constructor not defined
+	void operator=(const MTRand53&); // assignment operator not defined
+};
+unsigned long MTRand_int32::state[n] = { 0x0UL };
+int MTRand_int32::p = 0;
+bool MTRand_int32::init = false;
+
+void MTRand_int32::gen_state() { // generate new state vector
+	for (int i = 0; i < (n - m); ++i)
+		state[i] = state[i + m] ^ twiddle(state[i], state[i + 1]);
+	for (int i = n - m; i < (n - 1); ++i)
+		state[i] = state[i + m - n] ^ twiddle(state[i], state[i + 1]);
+	state[n - 1] = state[m - 1] ^ twiddle(state[n - 1], state[0]);
+	p = 0; // reset position
 }
 
-double Membership(Set* s, int a) {
-	PerformanceCalculator pc;
-	double avg1 = 0;
-	for (int k = 0; k < 3; k++) {
-		pc.start();
-		try {
-			s->Belongs(a);
-		}
-		catch (ErrNoElement g) {}
-		pc.stop();
-		avg1 += pc.elapsedMillis();
+void MTRand_int32::seed(unsigned long s) {  // init by 32 bit seed
+	state[0] = s & 0xFFFFFFFFUL; // for > 32 bit machines
+	for (int i = 1; i < n; ++i) {
+		state[i] = 1812433253UL * (state[i - 1] ^ (state[i - 1] >> 30)) + i;
+		// see Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier
+		// in the previous versions, MSBs of the seed affect only MSBs of the array state
+		// 2002/01/09 modified by Makoto Matsumoto
+		state[i] &= 0xFFFFFFFFUL; // for > 32 bit machines
 	}
-	return avg1 / 3;
+	p = n; // force gen_state() to be called for next random number
 }
 
-void WriteAll(ostream& it, double* arr) {
-	for (int k = 0; k < 5; k++) {
-		it << setprecision(5)<< setw(10) << arr[k]<<' ';
-		if (k == 4) it << endl;
+void MTRand_int32::seed(const unsigned long* array, int size) { // init by array
+	seed(19650218UL);
+	int i = 1, j = 0;
+	for (int k = ((n > size) ? n : size); k; --k) {
+		state[i] = (state[i] ^ ((state[i - 1] ^ (state[i - 1] >> 30)) * 1664525UL))
+			+ array[j] + j; // non linear
+		state[i] &= 0xFFFFFFFFUL; // for > 32 bit machines
+		++j; j %= size;
+		if ((++i) == n) { state[0] = state[n - 1]; i = 1; }
 	}
+	for (int k = n - 1; k; --k) {
+		state[i] = (state[i] ^ ((state[i - 1] ^ (state[i - 1] >> 30)) * 1566083941UL)) - i;
+		state[i] &= 0xFFFFFFFFUL; // for > 32 bit machines
+		if ((++i) == n) { state[0] = state[n - 1]; i = 1; }
+	}
+	state[0] = 0x80000000UL; // MSB is 1; assuring non-zero initial array
+	p = n; // force gen_state() to be called for next random number
 }
 
-void Performances() {										
-
-	string titles[] = { "Set size","formation","insertion","removal","search","intersection" };
-	int vr[] = { 200,500,1000,10000,100000,1000000 };
-	int gr[] = {150,400,850,9000,98000,999500};
-	double forml[6], insl[6], reml[6], searchl[6], interl[6];			//arrays of results for individual samples -- linked list
-	double formt[6], inst[6], remt[6], searcht[6], intert[6];			//arrays of results for individual samples -- BST
-	PerformanceCalculator pc;
-
-	for (int i = 0; i < 5; i++) {
-		List *l = new List(), *lpom= new List();
-		BST *b = new BST(), *bpom=new BST();
-		double* avg;
-		double avg1 = 0;
-		avg = Formation(l, lpom, b, bpom, vr[i]);
-		forml[i] = avg[0];
-		formt[i] = avg[1];	
-		delete avg;
-		int a = gr[i]*rand() / ((double)RAND_MAX);						//the same number is inserted in both representations
-		avg1 = Insertion(l, a);
-		insl[i] = avg1; 
-		avg1 = Insertion(b, a);
-		inst[i] = avg1;
-		a = gr[i] * rand() / ((double)RAND_MAX);
-		avg1 = Removal(l, a);
-		reml[i] = avg1;
-		avg1 = Removal(b, a);
-		remt[i] = avg1;
-		a = gr[i] * rand() / ((double)RAND_MAX);
-		avg1 = Membership(l, a);
-		searchl[i] = avg1;
-		avg1 = Membership(b, a);
-		searcht[i] = avg1;
-		avg1 = 0;
-		for (int k = 0; k < 3; k++) {
-			pc.start();
-			l->Intersection(*lpom);									
-			pc.stop();
-			avg1 += pc.elapsedMillis();
-		}
-		interl[i] = avg1/3;
-		avg1 = 0;
-		for (int k = 0; k < 3; k++) {
-			pc.start();
-			b->Intersection(*bpom);									
-			pc.stop();
-			avg1 += pc.elapsedMillis();
-		}
-		intert[i] = avg1/3;
-		delete l;
-		delete lpom;
-		delete b;
-		delete bpom;
-		cout << endl;
-	}
-	for (int k = 0; k < 6; k++) {							//list performance print
-		cout << setw(15) << titles[k] << ' ';
-		switch (k) {
-		case 0: for (int k = 0; k < 5; k++) {
-			cout << setw(11) << vr[k] ;
-			if (k == 4) cout << endl;
-		} 
-				break;
-		case 1: WriteAll(cout, forml); break;
-		case 2: WriteAll(cout, insl); break;
-		case 3: WriteAll(cout, reml); break;
-		case 4: WriteAll(cout, searchl); break;
-		case 5: WriteAll(cout, interl); break;
-		default: break;
-		}
-	}
-	cout << endl << endl;
-	for (int k = 0; k < 6; k++) {							//bst performance print
-		cout << setw(15) << titles[k] << ' ';
-		switch (k) {
-		case 0: for (int k = 0; k < 6; k++) {
-			cout << setw(11) << vr[k];
-			if (k == 4) cout << endl;
-		}
-				break;
-		case 1: WriteAll(cout, formt); break;
-		case 2: WriteAll(cout, inst); break;
-		case 3: WriteAll(cout, remt); break;
-		case 4: WriteAll(cout, searcht); break;
-		case 5: WriteAll(cout, intert); break;
-		default: break;
-		}
-	}
-	cout << endl;
-}
+#endif
